a6dc03acfac9db75b035504f44d04936db12437b
Mert Gulbahce <mertgulbahce@gmail.com>
2022-06-27 01:13:35 +0300

    First Commit

---
+++ DegiskenlerVeriTipleri/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        /* int kisaKenar = 10, uzunKenar = 20, alan, cevre;
+        alan = kisaKenar * uzunKenar;
+        cevre = 2 * (kisaKenar + uzunKenar);
+        System.out.println("Alan: " + alan);
+        System.out.println("Cevre: " + cevre); */
+
+        char c1 = 'J';
+        char c2 = 'A';
+        char c3 = 'V';
+        char c4 = 'A';
+        char toplam = (char) (c1 + c2 + c3 + c4);
+        System.out.print(toplam);
+    }
+}
